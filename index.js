74ce3669d12836324c10bd7cfdab1ae388d2d3a5
mrkgc29 <kimgavi.04@icloud.com>
2026-05-21 12:22:30 +0200

    Exercise done

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const form = document.querySelector("form");
+const grid = document.querySelector("#card-grid");
 
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  console.log("button pressed!")
+  const title = document.querySelector("#title").value;
+  const src = document.querySelector("#src").value;
+  const description = document.querySelector("#description").value;
+  const card = document.createElement("div");
+  card.className = "bg-white rounded-xl shadow-lg overflow-hidden hover:scale-125 transition-transform duration-300";
+  card.innerHTML = `
+    <img src="${src}" alt="${title}" class="w-full h-48 object-cover">
+    <div class="p-4 ">
+      <h3 class="text-xl font-bold text-gray-800 mb-2">${title}</h3>
+      <p class="text-gray-600 text-sm">${description}</p>
+    </div>
+  `;
+  grid.appendChild(card);
+  form.reset();
+  
 }
